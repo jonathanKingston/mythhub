626d873058cc1c695582586d7915e7bad748baf1
Jonathan Kingston <jonathan@jooped.co.uk>
2014-02-24 23:58:53 +0000

    Initial commit

---
+++ init.js
@@ -0,0 +1,22 @@
+var myth = require('myth');
+var fromTextElement = document.getElementById('from');
+var toTextElement = document.getElementById('to');
+var errorElement = document.getElementById('error');
+
+function updateCSS() {
+  var css = fromTextElement.value;
+  errorElement.innerHTML = '';
+  errorElement.className = 'hidden';
+  var converted = toTextElement.value;
+  try {
+    converted = myth(css);
+  } catch(e) {
+    errorElement.className = '';
+    errorElement.innerHTML = e.toString();
+  }
+  toTextElement.value = converted;
+}
+
+fromTextElement.addEventListener('change', updateCSS);
+fromTextElement.addEventListener('keyup', updateCSS);
+updateCSS();
